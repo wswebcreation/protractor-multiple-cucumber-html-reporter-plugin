32f178875f7dd3a2ab442f97e7de9ca4b112dc3b
Wim Selles <wim@wswebcreation.nl>
2019-06-02 10:36:48 +0200

    chore: update travis

--- test/cucumber/config/no-options.conf.js
+++ test/cucumber/config/no-options.conf.js
@@ -2,13 +2,13 @@ const path = require('path');
 const config = require('./protractor.shared.conf').config;
 
 config.capabilities = {
-    browserName: 'chrome',
-    shardTestFiles: false,
-    maxInstances: 2,
-    chromeOptions: {
-        args: ['disable-infobars']
+    'browserName': 'chrome',
+    'shardTestFiles': false,
+    'maxInstances': 2,
+    'goog:chromeOptions': {
+        args: ['--headless']
     },
-    deviceProperties: {
+    'deviceProperties': {
         browser: {
             name: 'chrome',
             version: 'latest'
