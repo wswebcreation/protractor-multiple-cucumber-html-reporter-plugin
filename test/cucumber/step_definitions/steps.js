75fafb7f79dd3297df13c4e8f144ee09f08f2c1a
Wim Selles <wim@wswebcreation.nl>
2019-06-02 09:20:27 +0200

    chore: upgrade dependencies

--- test/cucumber/step_definitions/steps.js
+++ test/cucumber/step_definitions/steps.js
@@ -1,15 +1,13 @@
-const defineSupportCode = require('cucumber').defineSupportCode;
+const { Given, When, Then } = require('cucumber');
 
-defineSupportCode(({ Given, When, Then }) => {
-    Given('I do something', () => Promise.resolve('I do something'));
+Given('I do something', () => Promise.resolve('I do something'));
 
-    Given('I foo', () => Promise.resolve('I foo'));
+Given('I foo', () => Promise.resolve('I foo'));
 
-    When('I verified it', () => Promise.resolve('I verified it'));
+When('I verified it', () => Promise.resolve('I verified it'));
 
-    When('I foo it', () => Promise.resolve('I foo it'));
+When('I foo it', () => Promise.resolve('I foo it'));
 
-    Then('a report will be created', () => Promise.resolve('a report will be created'));
+Then('a report will be created', () => Promise.resolve('a report will be created'));
 
-    Then('I foo the bar', () => Promise.resolve('I foo the bar'));
-});
+Then('I foo the bar', () => Promise.resolve('I foo the bar'));
